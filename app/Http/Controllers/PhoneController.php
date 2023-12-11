0dff509abc16324e08fa876ef197bf7d9b3a8ed9
mississppi <msspstudio@gmail.com>
2023-12-11 15:11:30 +0900

    phone画面作成

---
+++ app/Http/Controllers/PhoneController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PhoneController extends Controller
+{
+    public function index()
+    {
+        return view('phone.index');
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,6 +31,13 @@
                     </x-nav-link>
                 </div>
 
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('phone')">
+                        {{ __('Phone') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
